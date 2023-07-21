5d1b3bb3933a71eca6b42a5c8fe9da47824139bd
Ritik269 <96427152+Ritik269@users.noreply.github.com>
2023-07-21 23:48:23 +0530

    Add files via upload

---
+++ create-customer/create-customer.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Customer } from '../customer';
+import { CustomerService } from '../customer.service';
+
+@Component({
+  selector: 'app-create-customer',
+  templateUrl: './create-customer.component.html',
+  styleUrls: ['./create-customer.component.css']
+})
+export class CreateCustomerComponent implements OnInit {
+  customer: any = {};
+  errorMsg: string = "";
+  constructor(private custService: CustomerService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  createCustomer() {
+    console.log('Inside Component');
+    console.log(this.customer);
+    this.custService.createCustomer(this.customer).subscribe((data: Customer) => {
+      this.router.navigate(['/']).then();
+    },
+      (error) => {
+        this.errorMsg = error;
+        this.router.navigate(['/createcustomer/']).then();
+      });
+  }
+
+
+}
